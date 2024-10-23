89eff176632b03f372d4024b024350deed3e44af
NaraBBB <nara.bbb@yahoo.com>
2024-10-22 22:30:10 -0500

    Initial commit

---
+++ primitive.js
@@ -0,0 +1,31 @@
+//Primitive data types String/ number / boolean
+
+//String
+var friend = "Alex lives in IL";
+console.log(friend);//Alex lives in IL
+
+//SubString
+var text = "Hello, World";
+var result = text.substring(0,5);
+console.log(result);//Hello
+
+//numbers
+var num = 4;
+console.log(num);//4
+
+var num1 = 4+4;
+console.log(num1);//8
+
+//boolean
+var nums = 5<4;
+console.log(nums);//false
+
+var number = 6>5;
+console.log(number);//true
+
+//null
+
+
+
+//undefined
+
---
+++ test.js
@@ -0,0 +1,17 @@
+var name = 'nara';
+console.log(name);
+
+var friends = { 
+    alex:{
+    age: 32,
+    location: 'chicago'
+    },
+
+    sara:{
+        age: 33,
+        location: "las vegas"
+    }
+}
+console.table(friends);
+
+
